81e8a2f55c715e29d873c823b3d9e2405f1e6722
h3zTr0n <alison.mukoma@yahoo.com>
2017-09-12 11:14:16 +0200

    sanitized screencast app with some code and admin panel look and feel

--- screencast/admin.py
+++ screencast/admin.py
@@ -1,3 +1,33 @@
+#coding: utf-8
+from __future__ import absolute_import
+
 from django.contrib import admin
 
-# Register your models here.
+from .models import Subject, Category, Screencast
+
+class SubjectModelAdmin(admin.ModelAdmin):
+    list_display = ['subject', 'date']
+    list_filter = ('subject', 'date',)
+    search_fields = ('subject',)
+
+    class Meta:
+        model = Subject
+admin.site.register(Subject, SubjectModelAdmin)
+
+class CategoryModelAdmin(admin.ModelAdmin):
+    list_display = ['category', 'date']
+    list_filter = ('category', 'date',)
+    search_fields = ('category',)
+
+    class Meta:
+        model = Category
+admin.site.register(Category, CategoryModelAdmin)
+
+class ScreencastModelAdmin(admin.ModelAdmin):
+    list_display = ['title', 'categories', 'date']
+    list_filter = ('title', 'categories', 'date',)
+    search_fields = ('title', 'categories', 'date',)
+
+    class Meta:
+        model = Screencast
+admin.site.register(Screencast, ScreencastModelAdmin)
--- screencast/models.py
+++ screencast/models.py
@@ -2,34 +2,51 @@ from django.db import models
 from django.forms import ModelForm
 from django.utils import timezone
 
+from django.utils.translation import ugettext_lazy as _
 
 class Subject(models.Model):
-    subject = models.CharField(max_length=100)
-    date = models.DateField(default=timezone.now())
+    subject = models.CharField(_("Subject"), max_length=100)
+    date = models.DateField(_("Date"), default=timezone.now)
+
+    class Meta:
+        ordering = ['-date']
+        verbose_name = "SUBJECT"
+        verbose_name_plural = "SUBJECTS"
 
     def __str__(self):
         return self.subject
 
 
 class Category(models.Model):
-    category = models.CharField(max_length=100)
-    date = models.DateField(default=timezone.now())
+    category = models.CharField(_("Category"), max_length=100)
+    date = models.DateField(_("Date"), default=timezone.now)
+
+    class Meta:
+        ordering = ['-date']
+        verbose_name = "CATEGORY"
+        verbose_name_plural = "CATEGORIES"
 
     def __str__(self):
         return self.category
 
 
 class Screencast(models.Model):
-    title = models.CharField(max_length=300)
-    description = models.TextField()
+    title = models.CharField(_("Title"), max_length=300)
+    description = models.TextField(_("Description"), blank=True, null=True)
     subjects = models.ManyToManyField(Subject)
-    categories = models.ManyToManyField(Category)
-    cover = models.ImageField(upload_to='images')
-    video = models.FileField(upload_to='videos')
-    date = models.DateField(default=timezone.now())
+    categories = models.ForeignKey(Category)
+    cover = models.ImageField(_("Cover"), upload_to='images%Y/%m/%d')
+    video = models.FileField(_("Video"), upload_to='videos%Y/%m/%d')
+    date = models.DateField(_("Date"), default=timezone.now)
     """docstring for Lesson"""
 
+    class Meta:
+        ordering = ['-date']
+        verbose_name = "SCREENCAST"
+        verbose_name_plural = "SCREENCASTS"
 
+    def __str__(self):
+        return self.title
 
 
 # # Create your models here.
